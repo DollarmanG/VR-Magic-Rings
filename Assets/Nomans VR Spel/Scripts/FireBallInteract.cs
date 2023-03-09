170d55434019aec21e8ccf33b342419d9cfad76e
DollarmanG <noman.syed@yh.nackademin.se>
2023-03-09 18:52:26 +0100

    added iceball, rings and made prefabs

--- Assets/Nomans VR Spel/Scripts/FireBallInteract.cs
+++ Assets/Nomans VR Spel/Scripts/FireBallInteract.cs
@@ -14,7 +14,21 @@ public class FireBallInteract : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         //this makes sure that the collision between the object and the player does not collide with eachother. if this was not set like this, the object would endlessly do a loop of the code below(respawn)
-        if (!collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Ring"))
+        {
+            //sets the velocity of the object (fireball in this case) to 0, when it respawns so that when the player throws the fireball, when it respawns, it doesn't keep moving with the velocity it had before.
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            //respawns the object(fireball) to the reference respawnpoint it is set to.
+            gameObject.transform.position = respawnPoint.position;
+            gameObject.transform.rotation = respawnPoint.rotation;
+
+            //plays the audio once.
+            audioOnImpact.PlayOneShot(fireballImpactClip);
+        }
+
+        if (collision.gameObject.CompareTag("LevelObjects"))
         {
             //sets the velocity of the object (fireball in this case) to 0, when it respawns so that when the player throws the fireball, when it respawns, it doesn't keep moving with the velocity it had before.
             rb.velocity = Vector3.zero;
