80494f7fbfd408489e1479987680f9c87d9ddbfa
DollarmanG <noman.syed@yh.nackademin.se>
2023-03-15 11:36:45 +0100

    added visual feedback on hitting obj in menu

--- Assets/Nomans VR Spel/Scripts/LoadScene.cs
+++ Assets/Nomans VR Spel/Scripts/LoadScene.cs
@@ -5,15 +5,27 @@ public class LoadScene : MonoBehaviour
 {
     public string sceneName; // The name of the scene you want to load
     public float delayTime = 1.0f; // Delay time in seconds before loading the new scene
+    [SerializeField] private Material triggeredMaterial;
+
+    private Material defaultMaterial;
+    private Renderer renderer;
 
     private bool triggerEntered = false; // Flag to check if the trigger has been entered
     private float timer = 2.0f;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        defaultMaterial = renderer.material;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Fireball")) // Assuming your player has the tag "Player"
         {
             triggerEntered = true;
+            renderer.material = triggeredMaterial;
+
         }
     }
 
